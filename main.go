a9e9bdb08b7ce8a260891548927038b96cfb0611
shiki <nandaleoardana@gmail.com>
2024-09-21 16:02:32 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,17 +8,14 @@ import (
 )
 
 func main() {
-	// Connect to the database
+
 	database.Connect()
 	defer database.DB.Close()
 
-	// Create router
 	router := gin.Default()
 
-	// Define routes
 	routes.BookRoutes(router)
 	routes.CategoryRoutes(router)
 
-	// Run server
 	router.Run(":8080")
 }
